004b848d7201c17917c995ca798b37e965fe05ee
studentwsb <sansong@o2.pl>
2021-09-25 13:17:16 +0200

    cw 4

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,26 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
+        for(int i = 0 ;i <=50;i++)
+        {
+            if(i <0)
+                break;
+            System.out.println("break" + i);
+
+        }
+        for(int x = 5;x<2;x++)
+        {
+            if(x >=50)
+                continue;
+            System.out.println("continue" +x );
+        }
+        for (int z = 30;z>50;--z)
+        {
+            if(z==55)
+                return;
+            System.out.println("reurn" + z);
+        }
+
 
     }
 }
