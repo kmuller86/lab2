da28943eca71ee13f318958a4d26c0db7e467e62
studentwsb <sansong@o2.pl>
2021-09-25 13:09:55 +0200

    xxxxx

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,41 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" Podaj liczbe =");
+
+        switch (scan.nextInt())
+        {
+            case 1:
+            {
+                if(scan.nextInt() %2==0 ^ scan.nextInt() /1 ==0)
+                System.out.println(" liczba parzysta " + scan.nextInt()) ;
+            }
+            case 2:
+            { if(scan.nextInt() %7 ==0)
+                System.out.println( " liczba poczleilna przez 7" + scan.nextInt() );
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+
+
 
     }
 }
