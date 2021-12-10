8ab98e55c32e53ccff7ca3480473ad1d07e54147
Your Name <sansong@o2.pl>
2021-12-10 09:13:44 +0100

    cztery

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,29 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-        for(int i = 0 ;i <=50;i++)
+        for(int i = 1 ;i <=10;i++)
         {
-            if(i <0)
+            if(i ==4)
+            {
                 break;
+            }
             System.out.println("break" + i);
 
         }
-        for(int x = 5;x<2;x++)
+        for(int x = 1;x<2;x++)
         {
-            if(x >=50)
+            if(x ==50)
+            {
                 continue;
+            }
             System.out.println("continue" +x );
         }
-        for (int z = 30;z>50;--z)
+        for (int z = 1;z>50;z++)
         {
             if(z==55)
+            {
                 return;
+            }
             System.out.println("reurn" + z);
         }
 
