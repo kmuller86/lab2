0e03a655484ac422ded968fb928182d724bf7274
Your Name <sansong@o2.pl>
2021-12-10 09:07:08 +0100

    trzy

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Main {
@@ -13,22 +14,28 @@ public class Main {
           a) ta liczba jest parzysta --> podpowiedz switch(a % 2) {
           b) program który określi czy liczba jest podzielna przez 7
          */
-        Scanner scan = new Scanner(System.in);
-        System.out.println(" Podaj liczbe =");
 
-        switch (scan.nextInt())
+        int a;
+        Scanner console = new Scanner(System.in);
+        System.out.print("Wpisz liczbę: ");
+        a = console.nextInt();
+
+        switch (a % 2)
         {
-            case 1:
-            {
-                if(scan.nextInt() %2==0 ^ scan.nextInt() /1 ==0)
-                System.out.println(" liczba parzysta " + scan.nextInt()) ;
-            }
-            case 2:
-            { if(scan.nextInt() %7 ==0)
-                System.out.println( " liczba poczleilna przez 7" + scan.nextInt() );
-            }
-
+            case 0 :
+                System.out.println("Liczba jest PARZYSTA");
+                break;
+            case 1 :
+                System.out.println("Liczba jest NIEPARZYSTA");
+                break;
         }
+        if(a % 7 == 0){
+            System.out.println("Liczba " + a + " jest podzielna przez 7." );
+        } else {
+            System.out.println("Liczba " + a + " nie dzieli się przez 7.");
+        }
+        System.out.println("NEXT");
+        System.out.println(" ");
 
 
 
@@ -46,6 +53,30 @@ public class Main {
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
            b) oraz ma określić ile jest pozostałych liter, które nie należą do: 'o', 'p', 'x', 'z', 'q'
          */
+        String text = "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo";
+        int[] tablica = {0, 0, 0, 0, 0 };
+        for (char zmiennaChar : text.toCharArray()) {
+            switch (zmiennaChar) {
+                case 'o':
+                    ++tablica[0];
+                    break;
+                case 'p':
+                    ++tablica[1];
+                    break;
+                case 'x':
+                    ++tablica[2];
+                case 'z':
+                    ++tablica[3];
+                case 'y':
+                    ++tablica[4];
+
+                    break;
+            }
+        }
+        System.out.printf("\nLiczba występuje\no: %d - razy\np: %d - razy\nx: %d - razy\nz: %d - razy\ny: %d - razy",
+                tablica[0], tablica[1], tablica[2], tablica[3],tablica[4]);
+
+
 
 
 
