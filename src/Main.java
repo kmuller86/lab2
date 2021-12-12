81dd74dd72cb6f49d25d28b0dc9036b00e935e1b
Your Name <sansong@o2.pl>
2021-12-12 10:18:10 +0100

    6666

--- src/Main.java
+++ src/Main.java
@@ -19,11 +19,70 @@ public class Main {
                 elementów listy
           */
 
+            List<String> zwierzeta = new ArrayList<>();
+            Scanner scan = new Scanner((System.in));
+            System.out.print("Wpisz  zwierzęta : ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                zwierzeta.add(scan.nextLine());
+            }
+            System.out.println("zwierzeta: " + zwierzeta);
+
+
+            for (String zmienna : zwierzeta)
+            {
+                System.out.print(zmienna + " , ");
+            }
+            System.out.println(" ");
+
+            int p = 0;
+            while (p < Math.min(zwierzeta.size(), 2))
+            {
+                zwierzeta.remove(zwierzeta.size() - 1);
+                p++;
+            }
+
+            zwierzeta.add("Koń");
+            zwierzeta.add("Pies");
+            zwierzeta.add("Lama");
+
+            System.out.println("Zmodyfikowane : " + zwierzeta);
+            System.out.println("Wielkość listy: " + zwierzeta.size());
+
+            System.out.println("zwierzeta od tyłu:");
+            for (int a = zwierzeta.size() - 1; a >= 0; a--)
+            {
+                System.out.print(zwierzeta.get(a) + " , ");
+            }
+
+            System.out.println(" ");
+            System.out.println("zwierzeta od tyłu : " + zwierzeta);
+
+
+
+
 
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        SortedSet<Integer> liczba = new TreeSet<>();
+        Scanner scan1 = new Scanner((System.in));
+        System.out.print("Wypisz kilka liczb max 10 : ");
+
+        for (int i = 0; i < 10; i++)
+        {
+            liczba.add(Integer.valueOf(scan.nextLine()));
+
+
+        }
+       // Collections.sort(liczba);
+        // nie wiem czemu ale nie dziala mi w taki sposob posortowanie jak w przykladzie :/
+        //zastosowałem SortedSet
+
+        System.out.println("Twoje Liczby to: " + liczba );
+
 
     }
 
